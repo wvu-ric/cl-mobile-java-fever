d171212ca316aecd2f0fe2a27f155c7cd14cf2fc
matthewbbrumley <matt@syntropy.io>
2016-02-02 13:55:22 -0500

    Initial

---
+++ fever/Fever.java
@@ -0,0 +1,26 @@
+
+public class Fever {
+    
+    //Variable Declaration
+    
+    private static double userTemperature;
+    
+    
+    
+    //Method Declaration
+    //main is the function that runs first and starts the lifecycle
+    public static void main(String[] args) {
+        
+        //We are not taking input in this exercise
+        
+        userTemperature = 98.6;
+        
+        //Use if-else logic to determine proper response
+        
+        System.out.println("Your temperature is low.");
+        System.out.println("Your temperature is normal.");
+        System.out.println("Your temperature is high.");
+        
+    }
+    
+}
